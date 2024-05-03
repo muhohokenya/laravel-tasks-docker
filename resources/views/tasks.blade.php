74dce07694c0d1f6aa5be4f99367c5016b2717b5
muhohoweb <muhohokenya@users.noreply.github.com>
2024-05-03 14:52:24 +0300

    Build from dev->codepipline->codedeploy

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Add New Task
+                    Add New Task from dev
                 </div>
 
                 <div class="panel-body">
