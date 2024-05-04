9beb799a6911653bd8d1b05db68565f104dce417
muhohoweb <muhohokenya@users.noreply.github.com>
2024-05-04 21:46:38 +0300

    Demo Code Deploy

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Add New Task from dev (Delete instances after 10 Min)
+                    Add New Task from dev
                 </div>
 
                 <div class="panel-body">
